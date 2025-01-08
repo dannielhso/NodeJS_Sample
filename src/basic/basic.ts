bd582b8bd75a8325007ce2934315efd4609ae90d
dannielhso <dannielha@naver.com>
2025-01-08 16:02:30 +0900

    타입 스크립트의 다양한 서포트

---
+++ src/basic/basic.ts
@@ -0,0 +1,28 @@
+const message = "Hello!";
+// Accessing the porperty 'toLowerCase'
+// on 'message' and the calling it
+message.toLowerCase();
+// Calling 'message'
+//message(); //변수의 타입만이 아닌 속성까지 체크해준다.
+
+
+const announcement = "Hello World!";
+// How quickly cna you spot the types?
+// announcement.toLocalLowercase();
+// announcement.toLocaleLowerCase();
+
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
+
+
+const value = Math.random() < 0.5 ? "a" : "b"; // 삼항연산자 true : false
+if (value !== "a") {
+  // ...
+} else if (value === "a") {
+  // Oops, unreachable
+}
+
+function flipCoin(){
+    // Meant to be Math.random()
+    return Math.random() < 0.5
+}

effccc6d3e76b5079499ddf4235012cf1551531a
dannielhso <dannielha@naver.com>
2025-01-08 12:39:50 +0900

    타입스크립트 파일 생성 및 컴파일

--- app.ts
+++ app.ts
@@ -1,18 +1,30 @@
-console.log("Hello");
-var a = 10; //숫자 number
-var b = "Hello"; //문자열 String
-var c = true; //참,거짓 Boolean
-var d = [1, 2, 3, 4]; //배열 Array 
-var e = { name: "Tom", age: 35 }; //객체 Object 
-a = 50;
+console.log("Hello")
+//명시적 타입 선언언
+let a:number = 10;//숫자 number
+let b:string = "Hello";//문자열 String
+let c:boolean = true;//참,거짓 Boolean
+let d:number[] = [1,2,3,4];//배열 Array 
+let e:Person = { name : "Tom", age : 35};//객체 Object 
+//참조변수는 모두 Object로 인식한다.
+
+interface Person{
+    name:string;
+    age:number;
+}
+
+a = 55;
 b = "50";
+
 console.log("a : " + a, ", typeof " + typeof a);
 console.log("b : " + b, ", typeof " + typeof b);
 console.log("c : " + c, ", typeof " + typeof c);
-console.log("d[0] : " + d[0], ", d[1] : " + d[1], ", d[2] : " + d[2], ", d[3] : " + d[3], ", typeof " + typeof d);
+console.log("d[0] : " + d[0], ", d[1] : " + d[1], ", d[2] : " + d[2], ", d[3] : " + d[3],", typeof " + typeof d);
 console.log("e.name : " + e.name, ", e.age : " + e.age, ", typeof " + typeof e);
+
 //예시
-var columnLength = 10;
+
+let columnLength = 10;
+
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
@@ -22,14 +34,19 @@ var columnLength = 10;
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
+
 columnLength = 30;
+
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
-var addLength = columnLength + 5;
-var meterLength = addLength / 100;
+
+let addLength = columnLength + 5;
+let meterLength = addLength / 100;
+
 //.. 개발 진행 과정..
 //.. 개발 진행 과정..
 //.. 개발 진행 완료
+
 console.log(addLength);
 console.log(meterLength);

2d85ed8ba81069fa026703f941a2cab8f0618cdc
dannielhso <dannielha@naver.com>
2025-01-08 12:13:00 +0900

    순수 자바 스크립트의 동적 타입 지정 예시

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Hello Node.js")
+console.log("Hello")
+
+let a = 10;//숫자 number
+let b = "Hello";//문자열 String
+let c = true;//참,거짓 Boolean
+let d = [1,2,3,4];//배열 Array 
+let e = { name : "Tom", age : 35};//객체 Object 
+//참조변수는 모두 Object로 인식한다.
+
+b = 50;
+
+console.log("a : " + a, ", typeof " + typeof a);
+console.log("b : " + b, ", typeof " + typeof b);
+console.log("c : " + c, ", typeof " + typeof c);
+console.log("d[0] : " + d[0], ", d[1] : " + d[1], ", d[2] : " + d[2], ", d[3] : " + d[3],", typeof " + typeof d);
+console.log("e.name : " + e.name, ", e.age : " + e.age, ", typeof " + typeof e);
+
+//예시
+
+let columnLength = 10;
+
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+
+columnLength = "30cm";
+
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+
+let addLength = columnLength + 5;
+let meterLength = addLength / 100;
+
+//.. 개발 진행 과정..
+//.. 개발 진행 과정..
+//.. 개발 진행 완료
+
+console.log(addLength);
+console.log(meterLength);
